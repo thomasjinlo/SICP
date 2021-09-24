ed7d89e56d76992198fb50e87bce508d857cf57c
Thomas Lo <thomasjinlo@gmail.com>
2021-09-24 12:24:29 -0500

    something

--- exercises-1.1.rkt
+++ exercises-1.1.rkt
@@ -33,3 +33,4 @@
         (cond ((and (<= a b) (<= a c)) (sum-of-squares b c))
               ((and (<= b a) (<= b c)) (sum-of-squares a c))
               ((and (<= c a) (<= c b)) (sum-of-squares a b))))
+
